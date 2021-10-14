5f41d468d4d73d4ab85be8ba37efafef5b6b3fff
Łukasz Ł <ukaszek1001@gmail.com>
2021-10-14 10:47:24 +0200

    dodano plik zawierajacy kod

---
+++ main.py
@@ -0,0 +1,93 @@
+# wychowawca = [
+# {'name': 'Krzysztof Baczynski'
+#  'klasy' : '1a 1b 2a'}
+#
+#
+# {'name': 'Jan Brzechwa'
+# 'klasy' :
+# '1c
+#  2b'}]
+#
+# nauczyciel = [
+# {'name': 'Jan Dlugosz'
+# 'przedmiot' : 'Język Polski'
+# 'klasy nauczania' :
+# '1a
+#  1b
+#  1c'}
+#
+# {'name': 'Maria Konopnicka'
+# 'przedmiot': Język Polski
+# 'klasy nauczania' :
+# '2a
+#  2b
+#  2c'}
+#
+# {'name': 'Stanisław Lem'
+# 'przedmiot': 'Fizyka'
+# 'klasy nauczania' :
+# '2a
+#  2b
+#  2c
+#  2d'}
+#
+# {'name': 'Henryk Sienkiewicz'
+# 'przedmiot': 'Historia'
+# 'klasy nauczania' :
+# '1a
+#  1b
+#  1d
+#  2a
+#  2b'}]
+#
+# uczen = [
+#  'name': 'Jan Brzechwa'
+#  'klasa': '1a'
+#
+# 'name': 'Jan Długosz'
+# 'klasa': '1a'
+#
+# 'name': 'Aleksander Fredro'
+# 'klasa': '1b'
+#
+# 'name':Witold Gombrowicz
+# 'klasa': '1b'
+#
+# 'name':Zbigniew Herbert
+# 'klasa': '1c'
+#
+# 'name':Hanna Krall
+# 'klasa': '2a'
+#
+# 'name':Zygmunt Krasiński
+# 'klasa': '2a'
+#
+# 'name': 'Jan Andrzej Morsztyn'
+# 'klasa': '2a'
+#
+# 'name': 'Małgorzata Musierowicz'
+# 'klasa': '2b'
+#
+# 'name': 'Cyprian Kamil Norwid'
+# 'klasa': '2b'
+#
+# 'name':'Eliza Orzeszkowa'
+# 'klasa' : '1a'
+#
+# 'name': 'Bolesław Prus'
+# 'name': 'Juliusz Słowacki'
+# 'klasa': '2c'
+#
+# 'name': 'Julian Tuwim'
+# 'klasa': '2c'
+#
+# 'name': 'Stanisław Wyspiański'
+# 'klasa': '2c'}]
+# koniec
+
+
+class WychowawcaKlasy:
+    def__init__(self, name, klasy):
+	self.name = name
+	self.klasy = klasy
+Wychowawca = WychowawcaKlasy()
